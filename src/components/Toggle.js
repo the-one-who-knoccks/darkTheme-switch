90696839a37eb75aab37623b59073a5ea9e3ef58
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2020-08-22 20:21:13 -0300

    :zap: Components
    :wq

---
+++ src/components/Toggle.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { func, string } from 'prop-types';
+import styled from 'styled-components';
+
+import { ReactComponent as MoonIcon } from '../icons/moon2.svg'
+import { ReactComponent as SunIcon } from '../icons/sun2.svg';
+
+const Toggle = ({ theme, toggleTheme }) => {
+  const isLight = theme === 'light';
+
+  return (
+    <ToggleContainer lightTheme={isLight} onClick={toggleTheme} >
+      <SunIcon />
+      <MoonIcon />
+    </ToggleContainer>
+  );
+};
+
+Toggle.propTypes = {
+  theme: string.isRequired,
+  toggleTheme: func.isRequired,
+}
+
+const ToggleContainer = styled.button`
+  background: ${({ theme }) => theme.gradient};
+  border: 2px solid ${({ theme }) => theme.toggleBorder};
+  border-radius: 30px;
+  outline: none;
+  cursor: pointer;
+  display: flex;
+  font-size: 0.5rem;
+  justify-content: space-between;
+  margin: 0 auto;
+  overflow: hidden;
+  padding: 0.5rem;
+  position: relative;
+  width: 8rem;
+  height: 4rem;
+
+  svg {
+    height: auto;
+    width: 2.5rem;
+    transition: all 0.3s linear;
+    
+    // sun icon
+    &:first-child {
+      transform: ${({ lightTheme }) => lightTheme ? 'translateY(0)' : 'translateY(100px)'};
+    }
+    
+    // moon icon
+    &:nth-child(2) {
+      transform: ${({ lightTheme }) => lightTheme ? 'translateY(-100px)' : 'translateY(0)'};
+    }
+  }
+`;
+
+
+
+export default Toggle;
+
+
